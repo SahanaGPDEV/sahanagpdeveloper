0e88cb99103a6478dcb48f459fd662a52cf01270
Sahana Gp <sahanagpdeveloper@gmail.com>
2026-01-19 00:18:52 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/sahanagpdeveloper/",   // ⭐ THIS IS THE FIX
   server: {
     host: "::",
     port: 8080,
